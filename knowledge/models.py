bd573b5b36e543ac0fbe1a6b1be282e784dbeba2
electr0n4ik <andreyshka3@gmail.com>
2023-10-05 14:12:35 +0300

    Steps complete:
    - create app knowledge;
    - create model Course;
    - create model Lesson.

---
+++ knowledge/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+
+    name = models.CharField(
+        max_length=127,
+        verbose_name='Название')
+    description = models.CharField(
+        max_length=255,
+        verbose_name='Описание')
+    preview = models.ImageField(
+        **NULLABLE,
+        upload_to='knowledge/images',
+        verbose_name='Картинка')
+
+    def __str__(self):
+        return f"Курс {self.name}"
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=127,
+        verbose_name='Название')
+    description = models.CharField(
+        max_length=255,
+        verbose_name='Описание')
+    preview = models.ImageField(
+        **NULLABLE,
+        upload_to='knowledge/images',
+        verbose_name='Картинка')
+    url_video = models.URLField(**NULLABLE, verbose_name='Ссылка на видео')
+
+    def __str__(self):
+        return f"Урок {self.name}"
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
--- users/models.py
+++ users/models.py
@@ -1,5 +1,33 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name='почта')
+    phone = models.CharField(
+        **NULLABLE,
+        unique=True,
+        verbose_name='телефон')
+    city = models.CharField(
+        **NULLABLE,
+        max_length=150,
+        verbose_name='город')
+    avatar = models.ImageField(
+        **NULLABLE,
+        upload_to='users/avatar',
+        verbose_name='аватар')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
